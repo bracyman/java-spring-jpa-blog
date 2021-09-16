e2ca32010935e6d917300849d33b5bbcce035431
John Bracy <john.bracy@gmail.com>
2021-09-15 22:55:47 -0500

    COrrected find by function

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import com.pluralsight.blog.model.Category;
 import com.pluralsight.blog.model.Post;
 
 @Component
 public interface PostRepository extends JpaRepository<Post,Long>{
 	
-	public List<Post> findByCategory(String category);
+	public List<Post> findByCategory(Category category);
 }
