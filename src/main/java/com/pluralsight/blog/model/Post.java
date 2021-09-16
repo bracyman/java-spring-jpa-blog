5de014e9fb34d250f548d6d2f31e1006b847b3ea
John Bracy <john.bracy@gmail.com>
2021-09-15 22:36:41 -0500

    Module 1

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -1,17 +1,30 @@
 package com.pluralsight.blog.model;
 
-import javax.persistence.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
+@Entity
 public class Post {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String lead;
+    @Lob
+    @Column(length = 1000000)
     private String body;
     private String author;
+    @Temporal(TemporalType.DATE)
     private Date date;
 
     public Post() {
